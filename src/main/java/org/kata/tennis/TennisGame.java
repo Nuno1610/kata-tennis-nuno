55c5aba01456b5e1ea326a526f71a69c48998ebb
Nuno1610 <nundelesc@alum.us.es>
2026-02-22 00:14:02 +0100

    feat: Logica de primer punto básica

--- src/main/java/org/kata/tennis/TennisGame.java
+++ src/main/java/org/kata/tennis/TennisGame.java
@@ -1,7 +1,20 @@
 package org.kata.tennis;
 
 public class TennisGame {
+
+    private int playerOnePoints = 0;
+
+    public void playerOneScores() {
+        playerOnePoints++;
+    }
+
     public String getScore() {
-        return "Love-Love";
+        if (playerOnePoints == 0) {
+            return "Love-Love";
+        }
+        if (playerOnePoints == 1) {
+            return "Fifteen-Love";
+        }
+        return null;
     }
 }
