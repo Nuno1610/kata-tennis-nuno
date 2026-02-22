f6569b48f6f141eb9b27e32e123a19ef69c43994
Nuno1610 <nundelesc@alum.us.es>
2026-02-22 01:00:05 +0100

    feat: implementacion minima de la lógica de sets

--- src/main/java/org/kata/tennis/TennisSet.java
+++ src/main/java/org/kata/tennis/TennisSet.java
@@ -1,4 +1,27 @@
 package org.kata.tennis;
 
 public class TennisSet {
+
+    private int playerOneGames = 0;
+    private int playerTwoGames = 0;
+
+    public String getScore() {
+        return playerOneGames + "-" + playerTwoGames;
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public String getWinner() {
+        return null;
+    }
+
+    public void playerOneWinsGame() {
+        playerOneGames++;
+    }
+
+    public void playerTwoWinsGame() {
+        playerTwoGames++;
+    }
 }
